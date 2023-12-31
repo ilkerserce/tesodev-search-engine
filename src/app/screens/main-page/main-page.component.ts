50b3b6ae107c0a54263f61b7d798505702f699d4
ilkerserce <ilkerpehlivan28@gmail.com>
2023-12-31 06:11:49 +0300

    Branding and Input Area added on main page.

--- src/app/screens/main-page/main-page.component.ts
+++ src/app/screens/main-page/main-page.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponentComponent } from '../../components/primary-button-component/primary-button-component.component';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [PrimaryButtonComponentComponent],
+  imports: [
+    PrimaryButtonComponentComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
-export class MainPageComponent {
 
+export class MainPageComponent {
+  searchKeywordForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.searchKeywordForm = this.fb.group({
+      searchKeyword: '',
+    })
+  }
+
+  ngOnInit() {
+    this.searchKeywordForm.valueChanges.subscribe((newValue) => {
+      console.log(newValue);
+    });
+  }
 }
