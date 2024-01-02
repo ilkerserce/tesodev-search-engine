6644251cfaad8f64731bcb786d8d5f05989e1fb6
ilkerserce <ilkerpehlivan28@gmail.com>
2024-01-02 07:40:36 +0300

    Search page header added.

--- src/app/screens/search-page/search-page.component.ts
+++ src/app/screens/search-page/search-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { PrimaryButtonComponentComponent } from '../../components/primary-button-component/primary-button-component.component';
+import { RouterLinkService } from '../../services/router-link-service';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [],
+  imports: [
+    PrimaryButtonComponentComponent,
+  ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
-export class SearchPageComponent {
 
+export class SearchPageComponent {
+  constructor(public routerLinkService: RouterLinkService) { }
 }
