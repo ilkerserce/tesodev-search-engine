8c9973641b8421fc2e55c8744af9af6881ae9c7c
ilkerserce <ilkerpehlivan28@gmail.com>
2024-01-03 04:32:02 +0300

    Custom select box created at search page.

--- src/app/screens/search-page/search-page.component.ts
+++ src/app/screens/search-page/search-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PrimaryButtonComponentComponent } from '../../components/primary-button-component/primary-button-component.component';
 import { RouterLinkService } from '../../services/router-link-service';
 
@@ -6,6 +7,7 @@ import { RouterLinkService } from '../../services/router-link-service';
   selector: 'app-search-page',
   standalone: true,
   imports: [
+    CommonModule,
     PrimaryButtonComponentComponent,
   ],
   templateUrl: './search-page.component.html',
@@ -14,4 +16,16 @@ import { RouterLinkService } from '../../services/router-link-service';
 
 export class SearchPageComponent {
   constructor(public routerLinkService: RouterLinkService) { }
+
+  selectedOption: string = 'Order By';
+  showOptionsFlag: boolean = false;
+
+  toggleOptions() {
+    this.showOptionsFlag = !this.showOptionsFlag;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.showOptionsFlag = false;
+  }
 }
